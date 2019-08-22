08c5ab0c908d4ebb0b0a3c84c10610e2e7687b94
ehanson94 <ehanson94@gmail.com>
2019-08-22 15:25:23 -0400

    updated creating user profile and profile page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import { LoginForm, UserProfile } from ".";
+import RegisterForm from "./RegisterForm";
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <LoginForm />} />
-        <Route exact path="/profile" render={() => <UserProfile />} />
+        <Route exact path="/" component={LoginForm} />
+        <Route exact path="/profile" component={UserProfile} />
+        <Route exact path="/register" component={RegisterForm} />
       </Switch>
     );
   }
