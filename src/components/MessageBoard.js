463748227eee790afda9d92a0e5f48bd976d0813
Jake Hershey <joehershey45@gmail.com>
2019-08-23 17:47:44 +0000

    Updated Styling

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getMessages } from "../actions";
+import { getMessages} from "../actions";
 
 class MessageBoard extends Component {
   componentDidMount() {
     this.props.getMessages();
+    //this.props.likeMessage();
   }
 
   render() {
@@ -13,19 +14,50 @@ class MessageBoard extends Component {
       <>
         <section>
           <div>
-            <p>This is the MessageBoard</p>
+            <div className="section">
+              <div className="container">
+                <h1 className="title is-1 has-text-centered">
+                  {" "}
+                  Message Board{" "}
+                </h1>
+              </div>
+            </div>
             {this.props.messages.map(message => {
               return (
                 <React.Fragment key={message.id}>
-                  <article class="message is-primary">
-                    <div class="message-header has-text-left-desktop">
-                      {" "}
-                      <img src="https://imgix.ranker.com/user_node_img/50088/1001747365/original/protect-from-daddy-and-_39_s-scary-face-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces" alt="img"/>
-                      Username: {message.username}
-                    </div>
+                  <div className="section">
+                    <div className="container">
+                      <div className="message is-primary">
+                        <div
+                          id="message"
+                          className="message-header is-clearfix"
+                        >
+                          {" "}
+                          <img
+                            src="https://imgix.ranker.com/user_node_img/50088/1001747365/original/protect-from-daddy-and-_39_s-scary-face-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces"
+                            alt="Bulma as default"
+                          />
+                          <h1 id="messageUsernames">{message.username} </h1>
+                        </div>
 
-                    <div class="message-body">Message Text: {message.text}</div>
-                  </article>
+                        <div id="message" className="message-body">
+                          {message.text}
+                        </div>
+                        <button className="button">
+                          {console.log(message.id)}
+                          <div id="like">
+                            Like
+                            {message.likes.map(like => {
+                              //   {
+                              //     onclick = this.props.likeMessage(message.id);
+                              //   }
+                              return <p key={like.id}>Liked by: {like.id}</p>;
+                            })}
+                          </div>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                   <br />
                 </React.Fragment>
               );
@@ -38,12 +70,19 @@ class MessageBoard extends Component {
 }
 
 const mapStateToProps = state => {
+  console.log(state);
   return {
     messages: state.messages.getMessages
   };
 };
 
+const mapDispatchToProps = {
+  getMessages
+  //likeMessage
+};
+
 export default connect(
   mapStateToProps,
-  { getMessages }
+  //{ getMessages }
+  mapDispatchToProps
 )(MessageBoard);
